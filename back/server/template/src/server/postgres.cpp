eb531f74426a91edf2ee5bea0030e4489fa2902c
tkd995 <tkd995@vols.utk.edu>
2025-04-07 21:59:50 -0400

    Create postgres.cpp

---
+++ back/server/template/src/server/postgres.cpp
@@ -0,0 +1,47 @@
+//sudo apt install libpqxx-dev
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <pqxx/pqxx>
+#include <chrono>
+
+using namespace std;
+/*
+check if username exists*
+if it does check if password matches
+if not create new user entry*
+
+add chat to room's table*
+
+create table for room*
+add list of users to usersinroom*
+
+query not stored msgs
+*/
+result addUsertoRoom(pqxx::work tx, string username, string password){//create user
+	return tx.exec("INSERT INTO users VALUES (" + username + "," + password + ");");
+}
+
+bool userExists(pqxx::word tx, string username){//check if user exists
+	return tx.exec("FROM users; WHERE [username] LIKE '" + username + "'").size();
+}
+
+result createUser(pqxx::word tx, string username, string password){//if username not used create it with given password
+	if(!userExists(tx, username)) return addUsertoRoom(tx, username, password);
+}
+
+result addChattoRoom(pqxx::work tx, string room, string username, string msgId, string msg){
+	return tx.exec("INSERT INTO room_" + room + " VALUES (" + msgId + "," + username + "," + msg + "," + ctime(chrono::system_clock::to_time_t(chrono::system_clock::now())) + ");");
+}
+
+bool createRoom(string name, vector<string> usersToAdd){//create new table for chats and add users to room table
+	//make sure these both run, if fail return false
+	tx.exec("CREATE TABLE room_<" + name + "> (chatId INT, username varchar(255), message varchar(4096), datetime timestamp[(p)][with time zone]);");
+	for(int i = 0; i < (int)usersToAdd.size(); i++){
+		tx.exec("INSERT INTO usersinrooms VALUES (" + name + "," + usersToAdd[i] + ");");
+	}
+
+	return true;
+}
