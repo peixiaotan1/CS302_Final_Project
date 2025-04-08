44be60b7c0bc62c84efae011e795d70db7490218
tkd995 <tkd995@vols.utk.edu>
2025-04-07 22:06:37 -0400

    Update postgres.cpp
    quick bug fixes, something seems wrong

--- back/server/template/src/server/postgres.cpp
+++ back/server/template/src/server/postgres.cpp
@@ -20,23 +20,23 @@ add list of users to usersinroom*
 
 query not stored msgs
 */
-result addUsertoRoom(pqxx::work tx, string username, string password){//create user
+pqxx::result addUsertoRoom(pqxx::work tx, string username, string password){//create user
 	return tx.exec("INSERT INTO users VALUES (" + username + "," + password + ");");
 }
 
-bool userExists(pqxx::word tx, string username){//check if user exists
+bool userExists(pqxx::work tx, string username){//check if user exists
 	return tx.exec("FROM users; WHERE [username] LIKE '" + username + "'").size();
 }
 
-result createUser(pqxx::word tx, string username, string password){//if username not used create it with given password
+pqxx::result createUser(pqxx::work tx, string username, string password){//if username not used create it with given password
 	if(!userExists(tx, username)) return addUsertoRoom(tx, username, password);
 }
 
-result addChattoRoom(pqxx::work tx, string room, string username, string msgId, string msg){
+pqxx::result addChattoRoom(pqxx::work tx, string room, string username, string msgId, string msg){
 	return tx.exec("INSERT INTO room_" + room + " VALUES (" + msgId + "," + username + "," + msg + "," + ctime(chrono::system_clock::to_time_t(chrono::system_clock::now())) + ");");
 }
 
-bool createRoom(string name, vector<string> usersToAdd){//create new table for chats and add users to room table
+bool createRoom(pqxx::work tx, string name, vector<string> usersToAdd){//create new table for chats and add users to room table
 	//make sure these both run, if fail return false
 	tx.exec("CREATE TABLE room_<" + name + "> (chatId INT, username varchar(255), message varchar(4096), datetime timestamp[(p)][with time zone]);");
 	for(int i = 0; i < (int)usersToAdd.size(); i++){
